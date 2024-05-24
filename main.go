887f7514e38c5de266f1416de200951c043c6782
Dmytro Movchan <mail@dmtmov.me>
2024-05-25 00:54:37 +0300

    add Char stringer, more comments

--- main.go
+++ main.go
@@ -6,28 +6,59 @@ import (
 )
 
 type States struct {
-	regular, correct, wrong, focus string
+	regular string // the initial state. No modificators;
+	correct string // color the char into green;
+	wrong   string // color the char into red;
+	focus   string // add underline symbol to the char to mark as a cursor;
 }
 
-/*
-Describe atomic character in a text to type.
-
-	`value` contains the pressed key value for current iteration;
-	`original` stores the initial symbol in a text;
-	`state` can have one of States value;
-*/
+// Single character instance
 type Char struct {
-	value, original, state string
+	value    string // the new value set from the pressed key;
+	original string // the initial value taken from the placeholder;
+	state    string // chosen style to character;
 }
 
 // Update the state of reffered character
-func (char *Char) setState(val, stt string) string {
-	return val + stt
+func (char *Char) setState(val, stt string) {
+	char.value = val
+	char.state = stt
+}
+
+type Styles struct {
+	okBlue, fail, end, underline string
+	// HEADER := "\033[95m"
+	// OKBLUE = "\033[94m"
+	// OKCYAN = "\033[96m"
+	// OKGREEN = "\033[92m"
+	// WARNING = "\033[93m"
+	// FAIL = "\033[91m"
+	// ENDC = "\033[0m"
+	// BOLD = "\033[1m"
+	// UNDERLINE = "\033[4m"
+	// BLACK = "\033[97m"
 }
 
 // Return a character according its state at the moment of iteration
-func (char *Char) repr() string {
-	return char.value
+func (char Char) String() string {
+	// TODO: find more elegant solution to store styles
+	styles := Styles{
+		okBlue:    "\033[94m",
+		fail:      "\033[91m",
+		underline: "\033[4m",
+		end:       "\033[0m",
+	}
+
+	var reprVal string
+	switch char.state {
+	case styles.okBlue:
+		reprVal = fmt.Sprintf("%s%s%s", styles.okBlue, char.value, styles.end)
+	case styles.fail:
+		reprVal = fmt.Sprintf("%s%s%s", styles.fail, char.value, styles.end)
+	default:
+		reprVal = char.value
+	}
+	return reprVal
 }
 
 const placeholder = "Lorem Ipsum"
@@ -36,10 +67,11 @@ func main() {
 	cursor := 0
 	states := States{"r", "c", "w", "f"}
 
-	var charSeq []Char
+	var chars []Char
 
+	// TODO: is there any map() alternative from python?
 	for _, value := range placeholder {
-		charSeq = append(charSeq, Char{
+		chars = append(chars, Char{
 			value:    string(value),
 			original: string(value),
 			state:    states.regular,
@@ -52,49 +84,37 @@ func main() {
 	}
 	defer keyboard.Close()
 
-	// infinite loop
 	for {
 		char, key, err := keyboard.GetKey()
 		if err != nil {
 			panic(err)
 		}
 
-		cursorChar := &charSeq[cursor]
 		if key == keyboard.KeyBackspace2 {
 			cursor = max(0, cursor-1)
-            // TODO: use setValue with the appropriate Char refference
-			charSeq[cursor].state = states.regular
-			charSeq[cursor].value = charSeq[cursor].original
+			chars[cursor].setState(chars[cursor].original, states.regular)
 		} else {
 			if cursor <= len(placeholder) {
-				if string(char) == charSeq[cursor].original {
-					cursorChar.state = states.correct
-					cursorChar.value = string(char)
+				if string(char) == chars[cursor].original {
+					chars[cursor].setState(string(char), states.correct)
 				} else {
-					cursorChar.state = states.wrong
-					cursorChar.value = string(char)
+					chars[cursor].setState(string(char), states.wrong)
 				}
 				cursor += 1
 			}
 		}
 
-		// NOTE: add styling to test and print to stdout
-		var printSeq []string
-		for _, value := range charSeq {
-			printSeq = append(printSeq, value.repr())
-		}
-		fmt.Printf("\r%v", printSeq)
-
 		if cursor >= len(placeholder) || key == keyboard.KeyEsc {
 			break
 		}
+
+		// NOTE: add styling to test and print to stdout
+		fmt.Printf("\r%v", chars)
 	}
 
+	// TODO: replace with Stringer interface
 	for n := 0; n <= len(placeholder); n++ {
-
-		fmt.Println(charSeq)
 		if cursor == max(cursor, len(placeholder)) {
-			fmt.Println()
 			break
 		}
 	}
